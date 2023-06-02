9092740ccf0aa8ace4d9c22d632ac96e6b98ff11
zhangtianlin <zhangtianlin@ebaolife.com>
2023-06-02 17:22:45 +0800

    java项目初始化

---
+++ initsql/init.sql
@@ -0,0 +1,18 @@
+create table user
+(
+    id           bigint auto_increment comment '用户id'
+        primary key,
+    username     varchar(256)                           null comment '用户昵称',
+    userAccount  varchar(256)                           null comment '用户账号',
+    avatarUrl    varchar(1024)                          null comment '头像',
+    gender       tinyint                                comment '性别',
+    userPassword varchar(512)                           not null comment '密码',
+    phone        varchar(128)                           null comment '电话',
+    email        varchar(512)                           null comment '邮箱',
+    userStatus   int          default 0                 null comment '用户状态 0正常',
+    createTime   datetime     default CURRENT_TIMESTAMP null comment '数据插入时间',
+    updateTime   datetime     default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '数据更新时间',
+    isDelete     tinyint      default 0                 not null comment '逻辑删除(0未删除 1删除)'
+)
+    comment '用户表';
+
--- src/test/java/com/tianlin/usercenter/SampleTest.java
+++ src/test/java/com/tianlin/usercenter/SampleTest.java
@@ -2,18 +2,16 @@ package com.tianlin.usercenter;
 
 import com.tianlin.usercenter.mappers.UserMapper;
 import com.tianlin.usercenter.models.User;
-import org.junit.Assert;
-import org.junit.Test;
 
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test; // 如果直接用junit得test注解，会报错，需要用junit.jupiter.api.Test的注解
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class SampleTest {
 
     @Resource
@@ -23,7 +21,7 @@ public class SampleTest {
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
-        Assert.assertEquals(5, userList.size());
+        Assertions.assertEquals(5, userList.size()); // 断言，如果不等于5，就会报错.
         userList.forEach(System.out::println);
     }
 
