6662dc3fc9ae3261f8dc34e38e0811585b8e55a7
zhangtianlin <zhangtianlin@ebaolife.com>
2023-06-05 17:23:43 +0800

    java项目初始化

--- initsql/init.sql
+++ initsql/init.sql
@@ -1,19 +1,20 @@
-create table user
+create table if not exists tianlin.user
 (
     id           bigint auto_increment comment '用户id'
         primary key,
-    username     varchar(256)                           null comment '用户昵称',
+    username     varchar(256) default 'tianlin'         not null comment '用户昵称',
     userAccount  varchar(256)                           null comment '用户账号',
     avatarUrl    varchar(1024)                          null comment '头像',
-    gender       tinyint                                comment '性别',
+    gender       tinyint                                null comment '性别',
     userPassword varchar(512)                           not null comment '密码',
     phone        varchar(128)                           null comment '电话',
     email        varchar(512)                           null comment '邮箱',
     userStatus   int          default 0                 null comment '用户状态 0正常',
     createTime   datetime     default CURRENT_TIMESTAMP null comment '数据插入时间',
     updateTime   datetime     default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '数据更新时间',
-    isDelete     tinyint      default 0                 not null comment '逻辑删除(0未删除 1删除)'
-    userRole     int          default 0                 null comment '用户角色 0普通用户 1管理员'
+    isDelete     tinyint      default 0                 not null comment '逻辑删除(0未删除 1删除)',
+    userRole     int          default 0                 not null comment '用户角色 0 普通用户 1管理员',
+    userCode     varchar(512)                           not null comment '用户编码'
 )
     comment '用户表';
 
