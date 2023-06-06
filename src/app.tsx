15e3d1e0bad1781d88d6b6ed1b02cfce3ef06f08
zhangtianlin <221122030315@zjut.edu.cn>
2023-06-06 21:42:55 +0800

    feat(天琳): add '项目优化' option

--- src/app.tsx
+++ src/app.tsx
@@ -139,7 +139,7 @@ const handelResponse = async (response: Response): Promise<any> => {
   // 其他错误的抛出，最好是异步错误，这样可以被内部封装的request捕获。然后还得是错误对象，这样才能被全局错误处理捕获
   return Promise.reject(new Error(res?.description || res?.msg || '网络错误'));
 };
-const prefix = process.env.NODE_ENV === 'production' ? 'https://localhost:8080' : '/api'; // 开发环境下，代理到本地后端服务
+const prefix = process.env.NODE_ENV === 'production' ? 'https://114.132.229.206:8080' : '/api'; // 开发环境下，代理到本地后端服务
 export const request: RequestConfig = {
   prefix,
   timeout: 1000 * 10,
