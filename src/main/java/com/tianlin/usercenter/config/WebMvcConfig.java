36d694af80e4fb1caed16afe46ac2d4ea577006c
zhangtianlin <zhangtianlin@ebaolife.com>
2023-06-19 11:18:52 +0800

    修改session登录bug

--- src/main/java/com/tianlin/usercenter/config/WebMvcConfig.java
+++ src/main/java/com/tianlin/usercenter/config/WebMvcConfig.java
@@ -14,11 +14,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 //设置允许跨域请求的域名
                 //当**Credentials为true时，**Origin不能为星号，需为具体的ip地址【如果接口不带cookie,ip无需设成具体ip】
-                .allowedOrigins("http://114.132.229.206","http://localhost:8000","http://localhost:8080")
+                .allowedOrigins("http://127.0.0.1:5173", "http://127.0.0.1:8000")
                 //是否允许证书 不再默认开启
                 .allowCredentials(true)
                 //设置允许的方法
                 .allowedMethods("*")
+                //允许的请求头
+                .allowedHeaders("*")
                 //跨域允许时间
                 .maxAge(3600);
     }
